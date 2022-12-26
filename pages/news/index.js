6211ea0d53bd6888bde85b123d05559d7390d59f
doorahmie <doorahmie@gmail.com>
2022-12-26 14:36:17 +0900

    Show a example of page relocation. We have  better ways in NextJS.

--- pages/news/index.js
+++ pages/news/index.js
@@ -1,7 +1,27 @@
 // domain.com/news
-
+import { Fragment } from "react";
 function NewsPage() {
-  return <h1>The News Page</h1>;
+  return (
+    /**
+     * a 태그로 페이지 이동을 할 시,
+     * spa의 장점을 모두 잃는다.
+     * 새로운 페이지를 서버에 요청하는 것이기 때문이다.
+     *
+     * 또한 편한 예시를 들기 위해 li 태그를 하드코딩으로 작성했으나,
+     * 이런 부분이 대표적으로 서버에서 불러오는 부분임을 기재한다.
+     *
+     */
+    <Fragment>
+      <h1>The News Page</h1>
+      <ul>
+        <li>NextJS IS A Great Framework</li>
+        <li>
+          <a href="/news/something-important">Something Important</a>
+        </li>
+        <li>Something else </li>
+      </ul>
+    </Fragment>
+  );
 }
 
 export default NewsPage;
