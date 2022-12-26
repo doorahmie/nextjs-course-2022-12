8f546035e89e52047bbbd178cf3a5d103691dfd1
doorahmie <doorahmie@gmail.com>
2022-12-26 13:35:02 +0900

    It's possible to get informations from URLs.

--- pages/news/[newsId].js
+++ pages/news/[newsId].js
@@ -1,6 +1,12 @@
 // domain.com/news
+import { useRouter } from "next/router";
 
 function SomthingDynamicPage() {
+  const router = useRouter();
+  //   console.log(router.query.newsId);
+  const newsId = router.query.newsId;
+  console.log({ newsId });
+
   return <h1>The Something Dynamic Page</h1>;
 }
 
